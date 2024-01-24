bafe70479e4aabda54d372e6977bffbdc5ce4701
박진영 <am030813@naver.com>
2024-01-24 16:25:41 +0900

    AA

--- src/main/java/com/personal/myboard/controller/MBController.java
+++ src/main/java/com/personal/myboard/controller/MBController.java
@@ -1,15 +1,15 @@
-package com.personal.myboard.controller;
+// package com.personal.myboard.controller;
 
-import ch.qos.logback.core.model.Model;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+// import ch.qos.logback.core.model.Model;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
 
-@Controller
-public class MBController {
-    @GetMapping("https://jinyoun9.github.io")
-    public String boardList(@PathVariable String username, Model model){
-        return "index";
-    }
+// @Controller
+// public class MBController {
+//     @GetMapping("jinyoun9.github.io")
+//     public String boardList(@PathVariable String username, Model model){
+//         return "index";
+//     }
 
-}
+// }
