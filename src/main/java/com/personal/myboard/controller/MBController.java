ff638d4706f90127510070e26536ce2022f21376
박진영 <am030813@naver.com>
2024-01-24 15:49:50 +0900

    Controller, Index

--- src/main/java/com/personal/myboard/controller/MBController.java
+++ src/main/java/com/personal/myboard/controller/MBController.java
@@ -1,19 +1,13 @@
 package com.personal.myboard.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MBController {
-    @GetMapping("jinyoun9.github.io/list")
+    @GetMapping("jinyoun9.github.io/index")
     public String boardList(){
-        return "boardlist";
+        return "index";
     }
 
 }
