576f7954fcfe36f664466aff22c9bf4f081faf9e
박진영 <am030813@naver.com>
2024-01-24 15:29:18 +0900

    Controller, Boardlist

--- src/main/java/com/personal/myboard/controller/MBController.java
+++ src/main/java/com/personal/myboard/controller/MBController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MBController {
-    @GetMapping("/list")
+    @GetMapping("jinyoun9.github.io/list")
     public String boardList(){
         return "boardlist";
     }
